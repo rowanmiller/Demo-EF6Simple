520ba1668e8ca40042e5ebf21f2966621807b5ec
RoMiller <rowmil@microsoft.com>
2014-02-06 09:15:53 -0800

    Adding QuoteOfTheDay project to build NuGet package used in demo

---
+++ QuoteOfTheDay/QuoteOfTheDay/Quotes.cs
@@ -0,0 +1,15 @@
+﻿using QuoteOfTheDay.Properties;
+using System;
+
+namespace QuoteOfTheDay
+{
+    public static class Quotes
+    {
+        public static string GetQuote()
+        {
+            var index = new Random().Next(1, 4);
+            var quote = Resources.ResourceManager.GetString("Quote" + index);
+            return quote;
+        }
+    }
+}
